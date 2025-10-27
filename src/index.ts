d2905fad128bfd8e3fb915ce3d3723afb58ad994
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2025-10-27 13:54:37 +0300

    build: add modes for localhost and domain

--- src/index.ts
+++ src/index.ts
@@ -8,49 +8,58 @@ import config from "../config.json" assert { type: "json" };
 
 import "dotenv/config";
 
-const port = process.env.PORT;
+const port = process.env.PORT ?? 7300;
+const serverMode = process.env.SERVER_MODE ?? "localhost";
 const server = express();
 
 const loadApps = async () => {
 	const appsDir = resolve(config.appsDir);
 	const appFolders = await getApps(appsDir);
 
-	try {
-		for (const appFolder of appFolders) {
-			try {
-				const appPath = join(
-					appsDir,
-					`${appFolder}/${appFolder}-backend/dist`,
-					config.appEntryFile
+	if (!appFolders.length) {
+		console.warn(
+			`No applications found to load. Check the apps directory:\n${appsDir}`
+		);
+		process.exit(1);
+	}
+
+	for (const appFolder of appFolders) {
+		try {
+			const appPath = join(
+				appsDir,
+				`${appFolder}/${appFolder}-backend/dist`,
+				config.appEntryFile
+			);
+
+			if (!existsSync(appPath)) {
+				console.info(
+					`Application entry file not found for "${appFolder}". The build may not have been completed. Skipping...`
 				);
-
-				if (!existsSync(appPath)) {
-					continue;
-				}
-
-				const path = pathToFileURL(appPath).href;
-				const appModule = await import(path);
-				const app = await appModule.server();
-				const subdomain = `${appFolder}.mlc.local`;
-				server.use(vhost(subdomain, app));
-				console.log(`Application ${appFolder} on subdomain ${subdomain}`);
-
-				// Temp default route
-				if (appFolder === config.defaultApp) {
-					server.use(vhost("mlc.local", app));
-					console.log(`Application ${appFolder} on default route`);
-				}
-			} catch (e) {
-				console.error(`Error loading application ${appFolder}:`, e);
+				continue;
 			}
+
+			const path = pathToFileURL(appPath).href;
+			const appModule = await import(path);
+			const app = await appModule.server();
+			const subdomain = `${appFolder}.${serverMode}`;
+
+			// Subdomain routes
+			server.use(vhost(subdomain, app));
+			console.log(`Application ${appFolder} on subdomain ${subdomain}`);
+
+			// Default route
+			if (appFolder === config.defaultApp) {
+				server.use(vhost(serverMode, app));
+				console.log(`Application ${appFolder} on default route`);
+			}
+		} catch (e) {
+			console.error(`Error loading application ${appFolder}:`, e);
 		}
-	} catch (e) {
-		console.error("Error loading applications:", e);
 	}
 };
 
 loadApps().then(() => {
 	server.listen(port, () => {
-		console.log(`Server running on port ${port}`);
+		console.log(`Server running on port ${port}, mode: ${serverMode}`);
 	});
 });
